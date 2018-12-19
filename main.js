9069de95addd2105b5d41d028d95736d11f07484
jaydeep apte <jaydeepresources@gmail.com>
2018-12-19 10:35:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function buttonHover(){
+    document.getElementById("paraId").innerText = new Date().toString();
+}
+
+function buttonOut(){
+    document.getElementById("paraId").innerText = "Hover on the button to see date";
+}
